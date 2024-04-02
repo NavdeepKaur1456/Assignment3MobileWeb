b75184a7274dee6991170f7e963e6eedae06d266
Navdeep Kaur <kau15187@sheridancollege.ca>
2024-04-01 20:55:39 -0400

    deatail component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Pet } from '../pets';
+import { PetsDataService } from '../pet-data.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  pet$: Observable<Pet | undefined> = of(undefined); // Initialized with an empty Observable
 
+  constructor(
+    private route: ActivatedRoute,
+    private petsDataService: PetsDataService
+  ) {}
+
+  ngOnInit(): void {
+    this.pet$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = Number(params.get('id'));
+        return this.petsDataService.getPet(id);
+      })
+    );
+  }
 }
