56b58c3c04c50efce143ee0b9f998e2ea6407473
Navdeep Kaur <kau15187@sheridancollege.ca>
2024-04-01 20:59:05 -0400

    list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PetsDataService } from '../pet-data.service';
+import { Pet } from '../pets';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  pets: Pet[] = [];
 
+  constructor(private petsDataService: PetsDataService) { }
+
+  ngOnInit(): void {
+    this.petsDataService.getPetList().subscribe(pets => this.pets = pets);
+  }
 }
