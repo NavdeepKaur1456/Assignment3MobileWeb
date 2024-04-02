b35b4d4356c8ff022522bf35ab01aab22eed7caf
Navdeep Kaur <kau15187@sheridancollege.ca>
2024-04-01 20:44:52 -0400

    all component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    DetailsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
